af47c1d5593876f97b8a09e5378cf2ce7a911b9c
wesleyl29 <wesleyl29@nycstudents.net>
2025-10-01 17:38:01 +0000

    third commit

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -2,69 +2,57 @@ package com.example;
 import java.util.Scanner;
 
 public class TipCalculator {
-    //WRITE YOUR PROGRAM IN calculateTip
-    public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
-        
-
-        //DO NOT DELETE ANY OF THE CODE BELOW      
-
+    public static String calculateTip(int people, int percent, double cost) { 
                 String result = "-------------------------------\n" +
-                       "Total bill before tip: $" + cost + "\n" +
-                       "Total percentage: " + percent  + "%\n" +
+                       "Total bill before tip: $"+cost+"\n" +
+                       "Total percentage: "+percent+"%\n" +
                        "Total tip: $" + Math.round(cost*0.01*percent*100.0)/100.0  + "\n" +
                        "Total Bill with tip: $" + Math.round((cost*0.01*percent+cost)*100.0)/100.0 + "\n" +
-                       "Per person cost before tip: $" + (cost/people) + "\n" +
+                       "Per person cost before tip: $" + Math.round((cost/people)*100.0)/100.0 + "\n" +
                        "Tip per person: $" + Math.round(cost*0.01*percent/people*100.0)/100.0 + "\n" +
                        "Total cost per person: $" + Math.round((cost*0.01*percent+cost)/people*100.0)/100.0 + "\n" +
-                       "-------------------------------\n" +
-                       "Items ordered: ";
+                       "-------------------------------\n";
                     //learned how to round to nearest hundredths from https://stackoverflow.com/questions/8825209/rounding-decimal-points
-
-        return result;
+    return result;
     }
 
     public static String extraCredit(int people, int percent, double cost) {
         Scanner scan = new Scanner(System.in);
-        String result = "Extra credit not implemented";
+        String result = "-------------------------------\n" +
+                "Total bill before tip: $"+cost+"\n" +
+                "Total percentage: "+percent+"%\n" +
+                "Total tip: $" + Math.round(cost*0.01*percent*100.0)/100.0  + "\n" +
+                "Total Bill with tip: $" + Math.round((cost*0.01*percent+cost)*100.0)/100.0 + "\n" +
+                "Per person cost before tip: $" + Math.round((cost/people)*100.0)/100.0 + "\n" +
+                "Tip per person: $" + Math.round(cost*0.01*percent/people*100.0)/100.0 + "\n" +
+                "Total cost per person: $" + Math.round((cost*0.01*percent+cost)/people*100.0)/100.0 + "\n" +
+                "-------------------------------\n";
+        result += "Items ordered:\n";
+
         boolean condition = true;
-    
-        //  COPY AND PASTE YOUR PROGRAM FROM calculateTip() HERE 
-        
-        // the while loop condition is checked,
-        // and if TRUE, runs the code inside.
-        // when the code inside is done running, the condition is rechecked,
-        // and the loop repeats as long as the condition remains TRUE.
-        // when the condition becomes FALSE, it stops
-        String itemList = "";
-        String item = "";
-        while (condition = true) {
-            //enter your code here 
-            System.out.println("Enter an item name or type '-1' to finish: ");
+        String item;
+        while (condition) {
             item = scan.nextLine();
-            if (item == "hi") {
+            if (item.equals("-1")) {
+                result += "-------------------------------\n";
                 condition = false;
-            }
-            else {
-                itemList = item + "\n";
+            } else {
+                result += item + "\n";
             }
         }
-
+        scan.close();
         return result;
     }
-    
-    
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
-        //try different values for people, percent, and cost to test your program before running test cases
         int people=2; 
         int percent=5;
         double cost=29.56;              
-        //System.out.println(calculateTip(people,percent,cost));
-        System.out.println(extraCredit(people, percent, cost));
-        String result = calculateTip(people, percent, cost);
+        String result = extraCredit(people, percent, cost);
+        System.out.println(calculateTip(people,percent,cost));
         System.out.println(result);
-        result = extraCredit(people, percent, cost);
-        System.out.println(result);
+        
+        
     }
 }
         
